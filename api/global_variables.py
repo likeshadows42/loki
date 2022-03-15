f2c63878ef91cf6c1e0cee57a2a8acbe887c5e67
rodrigop <Rodrigop@comino-systems.com>
2022-03-15 12:41:52 +0000

    Adds global variables to share info in the API

---
+++ api/global_variables.py
@@ -0,0 +1,7 @@
+# ==============================================================================
+#                                GLOBAL VARIABLES
+# ==============================================================================
+
+models      = [] # stores all face verifier models
+rep_db      = [] # stores representation database
+faiss_index = [] # stores faiss index for verification speed up
