5840c9e15d8ea3b57fcf359e11deaca4a6d6c134
rodrigop <Rodrigop@comino-systems.com>
2022-03-15 12:47:43 +0000

    Adds more variables

--- api/global_variables.py
+++ api/global_variables.py
@@ -2,6 +2,7 @@
 #                                GLOBAL VARIABLES
 # ==============================================================================
 
-models      = [] # stores all face verifier models
-rep_db      = [] # stores representation database
-faiss_index = [] # stores faiss index for verification speed up
+models      = []     # stores all face verifier models
+rep_db      = []     # stores representation database
+db_changed  = False  # indicates if database has been modified and should be changed
+faiss_index = []     # stores faiss index for verification speed up
