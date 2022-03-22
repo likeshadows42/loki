1bb6c83d1875f19be5eef39bfcd5e2601749c422
rodrigop <Rodrigop@comino-systems.com>
2022-03-22 10:24:19 +0000

    Adds verify / recognition functionality

--- api/routers/recognition.py
+++ api/routers/recognition.py
@@ -16,7 +16,8 @@ from ..api_classes       import FaceVerifierParams, ImageSaveTypes,\
 from ..api_functions     import DST_ROOT_DIR, RAW_DIR, RDB_DIR,\
                                 build_face_verifier, create_reps_from_dir,\
                                 calc_embedding, get_embeddings_as_array,\
-                                calc_similarity
+                                calc_similarity, create_new_representation,\
+                                get_matches_from_similarity
 from .detection          import fd_router, FaceDetectorOptions
 from .verification       import fv_router
 
@@ -58,7 +59,7 @@ async def upload_files(files: List[UploadFile], overwrite = False,
         img_name = f.filename.split('.')[0]
         img_fp   = os.path.join(RAW_DIR, img_name + '.' + save_as)
 
-        if (img_name in all_files) or overwrite:
+        if not (img_name in all_files) or overwrite:
             if save_as == ImageSaveTypes.NPY:
                 np.save(img_fp, img[:, :, ::-1], allow_pickle=False,
                                                   fix_imports=False)
@@ -72,13 +73,13 @@ async def upload_files(files: List[UploadFile], overwrite = False,
 @fr_router.post("/debug/input")
 async def debug_inputs(params: VerificationParams):
     print('Input parameters:\n',
-          ' > detector_name'.ljust(18) , f': {params.detector_name}\n',
+          ' > detector_name'.ljust(18), f': {params.detector_name}\n',
           ' > verifier_name'.ljust(18), f': {params.verifier_name}\n',
-          ' > align'.ljust(18)         , f': {params.align}\n',
-          ' > normalization'.ljust(18) , f': {params.normalization}\n',
-          ' > metric'.ljust(18)        , f': {params.metric}\n',
-          ' > threshold'.ljust(18)     , f': {params.threshold}\n',
-          ' > verbose'.ljust(18)       , f': {params.verbose}\n', '', sep='')
+          ' > align'.ljust(18)        , f': {params.align}\n',
+          ' > normalization'.ljust(18), f': {params.normalization}\n',
+          ' > metric'.ljust(18)       , f': {params.metric}\n',
+          ' > threshold'.ljust(18)    , f': {params.threshold}\n',
+          ' > verbose'.ljust(18)      , f': {params.verbose}\n', '', sep='')
 
     return {'params':params}
 
@@ -87,21 +88,21 @@ async def debug_inputs(params: VerificationParams):
 @fr_router.post("/debug/inspect_globals")
 async def inspect_globals():
     #global app_models
-    print('[check_loaded_models] models:')
+    print('[inspect_globals] models:')
     for key, value in glb.models.items():
         print(f'   -> {key}'.ljust(16) + f': {value}')
     print('')
 
-    print('[check_loaded_models] database:')
+    print('[inspect_globals] database:')
     print('  - there are {} representations'.format(len(glb.rep_db)))
     for i, rep in zip(range(len(glb.rep_db)), glb.rep_db):
         print(f' Entry: {i}  |  ', end='')
         rep.show_summary()
 
-    print(f'[check_loaded_models] database change status: {glb.db_changed}')
-    print(f'[check_loaded_models] faiss index: {glb.faiss_index}')
+    print(f'[inspect_globals] database change status: {glb.db_changed}')
 
-    return {'models': list(glb.models.keys()), 'num_reps':len(glb.rep_db)}
+    return {'models': list(glb.models.keys()), 'num_reps':len(glb.rep_db),
+            'db_changed':glb.db_changed}
 
 
 # ------------------------------------------------------------------------------
@@ -157,7 +158,7 @@ async def recognize_single_face(tgt_file: UploadFile,
 # ------------------------------------------------------------------------------
 
 @fr_router.post("/create_database")
-async def create_database(params: CreateDatabaseParams,
+async def create_database(cdb_params: CreateDatabaseParams,
                           img_dir: Optional[str] = None,
                           db_path: Optional[str] = None,
                           force_create: Optional[bool] = False):
@@ -179,12 +180,12 @@ async def create_database(params: CreateDatabaseParams,
 
     elif len(glb.rep_db) == 0 or force_create:
         glb.rep_db = create_reps_from_dir(img_dir, glb.models,
-                                        detector_name=params.detector_name,
-                                        align=params.align, show_prog_bar=True,
-                                        verifier_names=params.verifier_names,
-                                        normalization=params.normalization,
-                                        tags=params.tags, uids=params.uids,
-                                        verbose=params.verbose)
+                                    detector_name=cdb_params.detector_name,
+                                    align=cdb_params.align, show_prog_bar=True,
+                                    verifier_names=cdb_params.verifier_names,
+                                    normalization=cdb_params.normalization,
+                                    tags=cdb_params.tags, uids=cdb_params.uids,
+                                    verbose=cdb_params.verbose)
 
     else:
         raise AssertionError('[create_database] Database should have a ' +  
@@ -208,67 +209,150 @@ async def create_database(params: CreateDatabaseParams,
 
 # ------------------------------------------------------------------------------
 
-@fr_router.post("/verify_file", response_model=VerificationMatches)
-async def verify_file(myfile: UploadFile, params: VerificationParams,
-                      add_to_database: Optional[bool] = False):
+from ..api_classes       import FaceVerifierOptions, NormalizationTypes,\
+                                DistanceMetrics
 
-    # Obtains contents of the file & transforms it into an image
-    data  = np.fromfile(myfile.file, dtype=np.uint8)
-    image = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
-    image = image[:, :, ::-1]
+@fr_router.post("/verify/no_upload", response_model=List[VerificationMatches])
+async def verify_no_upload(files: List[UploadFile],
+            detector_name: FaceDetectorOptions = FaceDetectorOptions.OPENCV,
+            verifier_name: FaceVerifierOptions = FaceVerifierOptions.VGGFace,
+            align        : bool                = True,
+            normalization: NormalizationTypes  = NormalizationTypes.BASE,
+            metric       : DistanceMetrics     = DistanceMetrics.COSINE,
+            threshold    : float               = -1):
+    
+    # Initializes results list and obtains the relevant embeddings from the 
+    # representation database
+    verification_results = []
+    dtb_embs             = get_embeddings_as_array(glb.rep_db, verifier_name)
 
-    print('verifier_name', params.verifier_name)
+    # Loops through each file
+    for f in files:
+        # Obtains contents of the file & transforms it into an image
+        data  = np.fromfile(f.file, dtype=np.uint8)
+        image = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+        image = image[:, :, ::-1]
 
-    # Calculate the face image embedding
-    region, embeddings = calc_embedding(image, glb.models,
-                                        align=params.align,
-                                        detector_name=params.detector_name, 
-                                        verifier_names=params.verifier_name,
-                                        normalization=params.normalization)
+        # Calculate the face image embedding
+        region, embeddings = calc_embedding(image, glb.models, align=align,
+                                            detector_name=detector_name, 
+                                            verifier_names=verifier_name,
+                                            normalization=normalization)
 
-    # Calculates the embedding of the current image and gets all embeddings
-    # (relevant to this model) from the database
-    print('embeddings', embeddings)
+        # Calculates the embedding of the current image
+        cur_emb  = embeddings[verifier_name]
 
-    cur_emb  = embeddings[params.verifier_name]
-    dtb_embs = get_embeddings_as_array(glb.rep_db, params.verifier_name)
+        # Calculates the similarity between the current embedding and all
+        # embeddings from the database
+        similarity_obj = calc_similarity(cur_emb, dtb_embs, metric=metric,
+                                         model_name=verifier_name,
+                                         threshold=threshold)
 
-    # Calculates the similarity between the current embedding and all embeddings
-    # from the database
-    similarity_obj = calc_similarity(cur_emb, dtb_embs, metric=params.metric,
-                                     model_name=params.verifier_name,
-                                     threshold=params.threshold)
+        # Gets all matches based on the similarity object and append the result
+        # to the results list
+        result = get_matches_from_similarity(similarity_obj, glb.rep_db,
+                                             verifier_name)
+        verification_results.append(result)
 
-    # Gets all matches
-    mtch_uids  = []
-    mtch_tags  = []
-    mtch_names = []
-    mtch_fps   = []
-    mtch_rgns  = []
-    mtch_embds = []
+    return verification_results
+    
+# ------------------------------------------------------------------------------
 
-    for i in similarity_obj['idxs']:
-        rep = glb.rep_db[i]
-        mtch_uids.append(rep.unique_id)
-        mtch_tags.append(rep.name_tag)
-        mtch_names.append(rep.image_name)
-        mtch_fps.append(rep.image_fp)
-        mtch_rgns.append(rep.region)
-        mtch_embds.append(rep.embeddings)
+@fr_router.post("/verify/with_upload", response_model=List[VerificationMatches])
+async def verify_with_upload(files: List[UploadFile],
+            detector_name: FaceDetectorOptions = FaceDetectorOptions.OPENCV,
+            verifier_name: FaceVerifierOptions = FaceVerifierOptions.VGGFace,
+            align        : bool                = True,
+            normalization: NormalizationTypes  = NormalizationTypes.BASE,
+            metric       : DistanceMetrics     = DistanceMetrics.COSINE,
+            threshold    : float               = -1,
+            save_as      : ImageSaveTypes      = ImageSaveTypes.NPY,
+            overwrite    : bool                = False):
+    
+    # Initializes results list, obtains the relevant embeddings from the 
+    # representation database and gets all files in the raw directory
+    verification_results = []
+    all_files            = os.listdir(RAW_DIR)
+    dtb_embs             = get_embeddings_as_array(glb.rep_db, verifier_name)
 
-    print(mtch_uids)
-    print(mtch_tags)
-    print(mtch_names)
-    print(mtch_fps)
-    print(mtch_rgns)
-    print(mtch_embds)
+    # Loops through each file
+    for f in files:
+        # Obtains contents of the file & transforms it into an image
+        data  = np.fromfile(f.file, dtype=np.uint8)
+        img   = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+        img   = img[:, :, ::-1]
 
-    return {'uids':mtch_uids, 'tags':mtch_tags, 'names':mtch_names,
-             'fps':mtch_fps , 'rgns':mtch_rgns, 'embds':mtch_embds}
+        # Obtains the file's image name and creates the full path
+        img_name = f.filename.split('.')[0]
+        img_fp   = os.path.join(RAW_DIR, img_name + '.' + save_as)
+
+        # Saves the image if it does not exist or if overwrite is True
+        if not (img_name in all_files) or overwrite:
+            if save_as == ImageSaveTypes.NPY:
+                np.save(img_fp, img, allow_pickle=False, fix_imports=False)
+            else:
+                mpimg.imsave(img_fp, img)
+
+        # Calculate the face image embedding
+        region, embeddings = calc_embedding(img, glb.models, align=align,
+                                            detector_name=detector_name, 
+                                            verifier_names=verifier_name,
+                                            normalization=normalization)
+
+        # Calculates the embedding of the current image
+        cur_emb  = embeddings[verifier_name]
+
+        # Calculates the similarity between the current embedding and all
+        # embeddings from the database
+        similarity_obj = calc_similarity(cur_emb, dtb_embs, metric=metric,
+                                         model_name=verifier_name,
+                                         threshold=threshold)
+
+        # Gets all matches based on the similarity object and append the result
+        # to the results list
+        result = get_matches_from_similarity(similarity_obj, glb.rep_db,
+                                             verifier_name)
+
+        # Chooses a tag automatically from the best match if it has a distance / 
+        # similarity of <=0.75*threshold
+        if len(result) > 0:
+            if similarity_obj['distances'][0]\
+                <= 0.75 * similarity_obj['threshold']:
+                tag = result['name_tags'][0]
+            else:
+                tag = ''
+        else:
+            tag = ''
+        print('Automatic tag:', tag)
+
+        # Creates a representation object for this image and adds it to the
+        # database
+        new_rep = create_new_representation(img_fp, region, embeddings, tag=tag)
+        glb.rep_db.append(new_rep)
+        glb.db_changed = True
+
+        # Stores the verification result
+        verification_results.append(result)
+
+    return verification_results
 
 # ------------------------------------------------------------------------------
 
+@fr_router.post("/debug/bug1", response_model=VerificationMatches)
+async def bug1(myfile: UploadFile, vf_params: VerificationParams):
+    # This bug occurs if a UploadFile ('myfile') is required as input
+    # If it is removed, then vf_params change depending on user input.
+    # If not, then vf_params ignores user's inputs and uses the default values.
+    # No idea why this happening.
 
+    print('Input parameters:\n',
+          ' > detector_name'.ljust(18), f': {vf_params.detector_name}\n',
+          ' > verifier_name'.ljust(18), f': {vf_params.verifier_name}\n',
+          ' > align'.ljust(18)        , f': {vf_params.align}\n',
+          ' > normalization'.ljust(18), f': {vf_params.normalization}\n',
+          ' > metric'.ljust(18)       , f': {vf_params.metric}\n',
+          ' > threshold'.ljust(18)    , f': {vf_params.threshold}\n',
+          ' > verbose'.ljust(18)      , f': {vf_params.verbose}\n', '', sep='')
 
 
 
