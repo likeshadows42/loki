93099d622a5a37292c7e83255f2a5aa67c1d7b61
rodrigop <Rodrigop@comino-systems.com>
2022-03-22 10:26:44 +0000

    Removes unnecessary global variable

--- api/global_variables.py
+++ api/global_variables.py
@@ -1,8 +1,7 @@
 # ==============================================================================
-#                                GLOBAL VARIABLES
+#                           APP-WIDE (GLOBAL) VARIABLES
 # ==============================================================================
 
-models      = []     # stores all face verifier models
-rep_db      = []     # stores representation database
-db_changed  = False  # indicates if database has been modified and should be changed
-faiss_index = []     # stores faiss index for verification speed up
+models      = []    # stores all face verifier models
+rep_db      = []    # stores representation database
+db_changed  = False # indicates whether database has been modified (and should be saved)
