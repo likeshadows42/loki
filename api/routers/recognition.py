29555f0a1e3ee688e3b6e88b927ca196042d85b0
rodrigop <Rodrigop@comino-systems.com>
2022-03-25 14:43:59 +0000

    Moves stuff around & implements new endpoint

--- api/routers/recognition.py
+++ api/routers/recognition.py
@@ -8,12 +8,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy                  as np
 import api.global_variables   as glb
 
+from io                       import BytesIO
 from typing                   import List, Optional
-from fastapi                  import APIRouter, UploadFile
+from zipfile                  import ZipFile
+from fastapi                  import APIRouter, UploadFile, File
+from IFR.classes              import *
 from deepface.DeepFace        import find
-from IFR.classes              import FaceVerifierParams, ImageSaveTypes,\
-                                    CreateDatabaseParams, VerificationParams,\
-                                    VerificationMatches
 from IFR.functions            import build_face_verifier, create_reps_from_dir,\
                                     calc_embedding, get_embeddings_as_array,\
                                     calc_similarity, create_new_representation,\
@@ -37,36 +37,6 @@ fr_router.face_verifier_name = None
 
 # ______________________________________________________________________________
 #                                  API METHODS
-# ------------------------------------------------------------------------------
-
-@fr_router.post("/upload_files")
-async def upload_files(files: List[UploadFile], overwrite = False,
-                       save_as: ImageSaveTypes = ImageSaveTypes.NPY):
-    """
-    API ENDPOINT: upload_files
-        Use this endpoint to upload several files at once. The files can be
-        saved as .png, .jpg or .npy files ([save_as='npy']). If the file already
-        exists in the data directory it is skipped unless the 'overwrite' flag
-        is set to True ([overwrite=False]).
-    """    
-    # Gets all files in the raw directory
-    all_files = os.listdir(img_dir)
-    
-    for f in files:
-        data     = np.fromfile(f.file, dtype=np.uint8)
-        img      = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
-        img_name = f.filename.split('.')[0]
-        img_fp   = os.path.join(img_dir, img_name + '.' + save_as)
-
-        if not (img_name in all_files) or overwrite:
-            if save_as == ImageSaveTypes.NPY:
-                np.save(img_fp, img[:, :, ::-1], allow_pickle=False,
-                                                  fix_imports=False)
-            else:
-                mpimg.imsave(img_fp, img[:, :, ::-1])
-
-    return {'message':'Images uploaded successfully.'}
-
 # ------------------------------------------------------------------------------
 
 @fr_router.post("/debug/input")
@@ -113,10 +83,57 @@ async def inspect_globals():
             'models': list(glb.models.keys()), 'num_reps':len(glb.rep_db),
             'db_changed':glb.db_changed}
 
+# ------------------------------------------------------------------------------
+
+@fr_router.post("/debug/bug1", response_model=VerificationMatches)
+async def bug1(myfile: UploadFile, vf_params: VerificationParams):
+    # This bug occurs if a UploadFile ('myfile') is required as input
+    # If it is removed, then vf_params change depending on user input.
+    # If not, then vf_params ignores user's inputs and uses the default values.
+    # No idea why this happening. #myfile: UploadFile, 
+
+    print('Input parameters:\n',
+          ' > detector_name'.ljust(18), f': {vf_params.detector_name}\n',
+          ' > verifier_name'.ljust(18), f': {vf_params.verifier_name}\n',
+          ' > align'.ljust(18)        , f': {vf_params.align}\n',
+          ' > normalization'.ljust(18), f': {vf_params.normalization}\n',
+          ' > metric'.ljust(18)       , f': {vf_params.metric}\n',
+          ' > threshold'.ljust(18)    , f': {vf_params.threshold}\n',
+          ' > verbose'.ljust(18)      , f': {vf_params.verbose}\n', '', sep='')
 
 # ------------------------------------------------------------------------------
 
-@fr_router.post("/recognize/single")
+@fr_router.post("/upload_files")
+async def upload_files(files: List[UploadFile], overwrite = False,
+                       save_as: ImageSaveTypes = ImageSaveTypes.NPY):
+    """
+    API ENDPOINT: upload_files
+        Use this endpoint to upload several files at once. The files can be
+        saved as .png, .jpg or .npy files ([save_as='npy']). If the file already
+        exists in the data directory it is skipped unless the 'overwrite' flag
+        is set to True ([overwrite=False]).
+    """    
+    # Gets all files in the raw directory
+    all_files = os.listdir(img_dir)
+    
+    for f in files:
+        data     = np.fromfile(f.file, dtype=np.uint8)
+        img      = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+        img_name = f.filename.split('.')[0]
+        img_fp   = os.path.join(img_dir, img_name + '.' + save_as)
+
+        if not (img_name in all_files) or overwrite:
+            if save_as == ImageSaveTypes.NPY:
+                np.save(img_fp, img[:, :, ::-1], allow_pickle=False,
+                                                  fix_imports=False)
+            else:
+                mpimg.imsave(img_fp, img[:, :, ::-1])
+
+    return {'message':'Images uploaded successfully.'}
+
+# ------------------------------------------------------------------------------
+
+@fr_router.post("/recognize/single") # uses deepface function 'find'
 async def recognize_single_face(tgt_file: UploadFile,
                                 params: FaceVerifierParams):
     """
@@ -166,35 +183,46 @@ async def recognize_single_face(tgt_file: UploadFile,
 
 # ------------------------------------------------------------------------------
 
-@fr_router.post("/create_database")
-async def create_database(cdb_params: CreateDatabaseParams,
-                          image_dir: Optional[str] = None,
-                          db_path: Optional[str] = None,
-                          force_create: Optional[bool] = False):
+@fr_router.post("/create_database/from_directory")
+async def create_database_from_directory(cdb_params: CreateDatabaseParams,
+                                         image_dir: Optional[str] = None,
+                                         db_dir: Optional[str] = None,
+                                         force_create: Optional[bool] = False):
+    # Initialize output message
+    output_msg = ''
 
-    # If image directory provided is None, use default directory
-    if not image_dir:
+    # If image directory provided is None or is not a directory, use default
+    # directory
+    if not image_dir or not os.path.isdir(image_dir):
         global img_dir
-        image_dir = img_dir
+        output_msg += 'Image dir is None, does not exist or is not a '\
+                   +  'directory. Using default directory instead.\n'
+        image_dir  = img_dir
 
-    # If database path provided is None, use default path
-    if not db_path:
+    # If database path provided is None or is not a directory, use default
+    # directory
+    if not db_dir or not os.path.isdir(db_dir):
         global rdb_dir
-        db_path = os.path.join(rdb_dir, 'rep_database.pickle')
+        output_msg += 'Database dir is None, does not exist or is not a '\
+                   +  'directory. Using default directory instead.\n'
+        db_dir = os.path.join(rdb_dir, 'rep_database.pickle')
 
     # Database exists (and has at least one element) and force create is False
     if len(glb.rep_db) > 0 and not force_create:
-        # Do nothing, return number of elements in the database?
-        pass
+        # Do nothing, but set message
+        output_msg += 'Database exists (and force create is False). '\
+                   +  'Skipping database creation.\n'
 
     elif len(glb.rep_db) == 0 or force_create:
-        glb.rep_db = create_reps_from_dir(image_dir, glb.models,
+        output_msg += 'Creating database: '
+        glb.rep_db  = create_reps_from_dir(image_dir, glb.models,
                                     detector_name=cdb_params.detector_name,
                                     align=cdb_params.align, show_prog_bar=True,
                                     verifier_names=cdb_params.verifier_names,
                                     normalization=cdb_params.normalization,
                                     tags=cdb_params.tags, uids=cdb_params.uids,
                                     verbose=cdb_params.verbose)
+        output_msg += 'success!\n'
 
     else:
         raise AssertionError('[create_database] Database should have a ' +  
@@ -214,21 +242,107 @@ async def create_database(cdb_params: CreateDatabaseParams,
     else:
         glb.db_changed = False
 
-    return {'length':len(glb.rep_db)}
+    return {'length':len(glb.rep_db), 'message':output_msg}
 
 # ------------------------------------------------------------------------------
 
-from IFR.classes       import FaceVerifierOptions, NormalizationTypes,\
-                                DistanceMetrics
+@fr_router.post("/create_database/from_zip")
+async def create_database_from_zip(myfile: UploadFile = File(...),
+    detector_name : FaceDetectorOptions       = default_detector,
+    verifier_names: List[FaceVerifierOptions] = [default_verifier],
+    align         : bool                      = default_align,
+    normalization : NormalizationTypes        = default_normalization,
+    tags          : Optional[List[str]]       = default_tags,
+    uids          : Optional[List[str]]       = default_uids,
+    verbose       : bool                      = default_verbose,
+    image_dir     : Optional[str]             = glb.IMG_DIR,
+    db_dir        : Optional[str]             = glb.RDB_DIR,
+    force_create  : Optional[bool]            = False):
+
+    # Initialize output message
+    output_msg = ''
+
+    # If image directory provided is None or is not a directory, use default
+    # directory
+    if not image_dir or not os.path.isdir(image_dir):
+        global img_dir
+        output_msg += 'Image dir is None, does not exist or is not a '\
+                   +  'directory. Using default directory instead.\n'
+        image_dir = img_dir
+
+    # If database path provided is None or is not a directory, use default
+    # directory
+    if not db_dir or not os.path.isdir(db_dir):
+        global rdb_dir
+        output_msg += 'Database dir is None, does not exist or is not a '\
+                   +  'directory. Using default directory instead.\n'
+        db_dir     = os.path.join(rdb_dir, 'rep_database.pickle')
+
+    # Database exists (and has at least one element) and force create is False
+    if len(glb.rep_db) > 0 and not force_create:
+        # Do nothing, but set message
+        output_msg += 'Database exists (and force create is False). '\
+                   +  'Skipping database creation.\n'
+
+    elif len(glb.rep_db) == 0 or force_create:
+        # Initialize dont_skip flag as True
+        dont_skip = True
+
+        # Extract zip files
+        output_msg += 'Extracting images in zip:'
+        try:
+            with ZipFile(BytesIO(myfile.file.read()), 'r') as myzip:
+                myzip.extractall(image_dir)
+            output_msg += ' success! '
+        except Exception as excpt:
+            dont_skip   = False
+            output_msg += f' failed (reason: {excpt}).'
+
+
+        # Create database
+        if dont_skip:
+            output_msg += 'Creating database: '
+            glb.rep_db  = create_reps_from_dir(image_dir, glb.models,
+                                            detector_name=detector_name,
+                                            align=align, show_prog_bar=True,
+                                            verifier_names=verifier_names,
+                                            normalization=normalization,
+                                            tags=tags, uids=uids,
+                                            verbose=verbose)
+            output_msg += 'success!\n'
+        else:
+            output_msg += 'Skipping database creation.\n'
+
+    else:
+        raise AssertionError('[create_database] Database should have a ' +  
+                             'length of 0 or more - this should not happen!')
+
+    # Modifies the database change flag (and sort the database if there is at
+    # least 1 element)
+    n_reps = len(glb.rep_db)
+    if n_reps == 1:
+        glb.db_changed = True
+
+    elif n_reps > 1:
+        # Sorts database and sets changed flag to True
+        glb.rep_db.sort(key=lambda x: x.image_name)
+        glb.db_changed = True
+
+    else:
+        glb.db_changed = False
+
+    return {'length':len(glb.rep_db), 'message':output_msg}
+
+# ------------------------------------------------------------------------------
 
 @fr_router.post("/verify/no_upload", response_model=List[VerificationMatches])
 async def verify_no_upload(files: List[UploadFile],
-            detector_name: FaceDetectorOptions = FaceDetectorOptions.OPENCV,
-            verifier_name: FaceVerifierOptions = FaceVerifierOptions.VGGFace,
-            align        : bool                = True,
-            normalization: NormalizationTypes  = NormalizationTypes.BASE,
-            metric       : DistanceMetrics     = DistanceMetrics.COSINE,
-            threshold    : float               = -1):
+            detector_name: FaceDetectorOptions = default_detector,
+            verifier_name: FaceVerifierOptions = default_verifier,
+            align        : bool                = default_align,
+            normalization: NormalizationTypes  = default_normalization,
+            metric       : DistanceMetrics     = default_metric,
+            threshold    : float               = default_threshold):
     
     # Initializes results list and obtains the relevant embeddings from the 
     # representation database
@@ -269,13 +383,13 @@ async def verify_no_upload(files: List[UploadFile],
 
 @fr_router.post("/verify/with_upload", response_model=List[VerificationMatches])
 async def verify_with_upload(files: List[UploadFile],
-            detector_name: FaceDetectorOptions = FaceDetectorOptions.OPENCV,
-            verifier_name: FaceVerifierOptions = FaceVerifierOptions.VGGFace,
-            align        : bool                = True,
-            normalization: NormalizationTypes  = NormalizationTypes.BASE,
-            metric       : DistanceMetrics     = DistanceMetrics.COSINE,
-            threshold    : float               = -1,
-            save_as      : ImageSaveTypes      = ImageSaveTypes.NPY,
+            detector_name: FaceDetectorOptions = default_detector,
+            verifier_name: FaceVerifierOptions = default_verifier,
+            align        : bool                = default_align,
+            normalization: NormalizationTypes  = default_normalization,
+            metric       : DistanceMetrics     = default_metric,
+            threshold    : float               = default_threshold,
+            save_as      : ImageSaveTypes      = default_image_save_type,
             overwrite    : bool                = False):
     
     # Initializes results list, obtains the relevant embeddings from the 
@@ -344,24 +458,6 @@ async def verify_with_upload(files: List[UploadFile],
         verification_results.append(result)
 
     return verification_results
-
-# ------------------------------------------------------------------------------
-
-@fr_router.post("/debug/bug1", response_model=VerificationMatches)
-async def bug1(myfile: UploadFile, vf_params: VerificationParams):
-    # This bug occurs if a UploadFile ('myfile') is required as input
-    # If it is removed, then vf_params change depending on user input.
-    # If not, then vf_params ignores user's inputs and uses the default values.
-    # No idea why this happening.
-
-    print('Input parameters:\n',
-          ' > detector_name'.ljust(18), f': {vf_params.detector_name}\n',
-          ' > verifier_name'.ljust(18), f': {vf_params.verifier_name}\n',
-          ' > align'.ljust(18)        , f': {vf_params.align}\n',
-          ' > normalization'.ljust(18), f': {vf_params.normalization}\n',
-          ' > metric'.ljust(18)       , f': {vf_params.metric}\n',
-          ' > threshold'.ljust(18)    , f': {vf_params.threshold}\n',
-          ' > verbose'.ljust(18)      , f': {vf_params.verbose}\n', '', sep='')
 
 # ------------------------------------------------------------------------------
 
