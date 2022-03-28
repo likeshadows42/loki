711dd84698204dba37c100a345255f401515fff8
lucag <lucag@comino-systems.com>
2022-03-28 16:46:07 +0100

    fix "/debug/input" endopint

--- api/routers/recognition.py
+++ api/routers/recognition.py
@@ -46,8 +46,8 @@ async def debug_inputs(params: VerificationParams):
           ' > metric'.ljust(18)       , f': {params.metric}\n',
           ' > threshold'.ljust(18)    , f': {params.threshold}\n',
           ' > verbose'.ljust(18)      , f': {params.verbose}\n', '', sep='')
-
-    return {'params':params}
+    
+    return params.dict()
 
 # ------------------------------------------------------------------------------
 
