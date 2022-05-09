05c6fd2e619b69720dab5082b854d9b2c2814e9d
lucag <lucag@comino-systems.com>
2022-05-09 23:11:07 +0100

    Add vuetify framwork

--- vue-loki/src/main.js
+++ vue-loki/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import axios from 'axios'
-// import store from './store'
-// import axios from 'axios'
+import { loadFonts } from './plugins/webfontloader'
 
-// const app = createApp(App).use(store).use(router)
+loadFonts()
+
 const app = createApp(App)
 app.config.globalProperties.axios=axios
-
-
+app.use(vuetify)
 app.mount('#app')
