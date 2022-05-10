ed7e81bd48e5c3f5fb6b69e5050009c903430c8d
lucag <lucag@comino-systems.com>
2022-05-10 09:50:36 +0100

    added manualk assign funciontality to ungroup faces page

--- vue-loki/src/main.js
+++ vue-loki/src/main.js
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+// import vuetify from './plugins/vuetify'
+import { createVuetify } from 'vuetify'
 import axios from 'axios'
 import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+
+loadFonts()
+
 app.config.globalProperties.axios=axios
 app.use(vuetify)
 app.mount('#app')
