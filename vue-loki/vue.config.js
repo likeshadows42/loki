7dc66d4159bbfa01f0f2eddd101462beeee5128f
lucag <lucag@comino-systems.com>
2022-05-19 17:31:06 +0100

    changed publicPath env variable for npm run

--- vue-loki/vue.config.js
+++ vue-loki/vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   },
 
   publicPath: 
-    process.env.NODE_ENV === 'test'
+    process.env.NODE_ENV === 'build'
       ? './'
       : 'http://localhost:8080/'
 })
