06dbf92052b7429fc87e00ce343670dcbeb3a579
lucag <lucag@comino-systems.com>
2022-05-23 23:45:25 +0100

    changed API prefix to "api/" instead of "fr/"

--- api/app.py
+++ api/app.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers     = ["*"],
 )
 
-app.include_router(fr_router, prefix="/fr", tags=["Face Recognition"])
+app.include_router(fr_router, prefix="/api", tags=["Face Recognition"])
 
 
 # ______________________________________________________________________________
