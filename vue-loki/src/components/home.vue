0b68cf8bf45c9ada2f76bcbc0faa5deb5ead090e
lucag <lucag@comino-systems.com>
2022-05-26 17:41:41 +0100

    fixed link to FastAPI docs in homepage

--- vue-loki/src/components/home.vue
+++ vue-loki/src/components/home.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <p>
       For a reference of Loki's APIs, please visit
-      <a href="https://git.local/irish-dev-team/loki" target="_blank" rel="noopener">Loki's official repo</a> or his <a href="http://127.0.0.1:8000/docs"  target="_blank">FastAPI's Swagger UI</a>.
+      <a href="https://git.local/irish-dev-team/loki" target="_blank" rel="noopener">Loki's official repo</a> or his <a href="/api/fr/docs"  target="_blank">FastAPI's Swagger UI</a>.
     </p>
   </div>
 </template>
