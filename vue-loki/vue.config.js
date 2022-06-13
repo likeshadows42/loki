ed56b14af432e31386293bd82eea16fe965bc571
lucag <lucag@comino-systems.com>
2022-06-13 12:37:50 +0100

    updated target localhost --> 127.0.0.1

--- vue-loki/vue.config.js
+++ vue-loki/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api/fr': {
-        target: 'http://localhost:8000/'
+        target: 'http://127.0.0.1:8000/'
       },
     }
   },
