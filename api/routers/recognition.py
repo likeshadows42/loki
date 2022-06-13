fe4037f5aa6652e84a5d2ec0268e96dd8c2e509e
rodrigop <Rodrigop@comino-systems.com>
2022-06-13 11:19:14 +0100

    Adds the backup_list endpoint (issue #49)

--- api/routers/recognition.py
+++ api/routers/recognition.py
@@ -564,6 +564,63 @@ async def restore_state(file_fp: str = Query(None, description="Backup zip file'
 
 # ------------------------------------------------------------------------------
 
+@fr_router.post("/utility/backup/list")
+async def backup_list(return_readable=True):
+    """
+
+    API endpoint: backup_list()
+
+    Lists all the available backup files in the backup directory specified by
+    the BACKUP_DIR global variable.
+    
+    If return_readable is True, then this function returns a dictionary where
+    each path is a seperate entry with the following sequential name structure:
+    path_ + i, e.g. path_001, path_002, etc.
+
+    If return_readable is False, then this function returns a dictionary with
+    the list of backup files' full paths under the 'backups' key.
+
+    Parameters:
+    - return_readable: toggles between returning a more 'readable' version of
+                        the backup files' full paths or simply a list with all
+                        paths [boolean, default=True].
+
+    Output:\n
+        The output of this functions depends on the value of the
+        'return_readable' parameter. In both cases, this function returns a
+        JSON-encoded dictionary with the following key/value pairs:
+            > If return_readable is True:
+                1. path_001: first backup file's full path [string].
+                2. path_002: second backup file's full path [string].
+                                        ...
+                n. path_(n): n-th backup file's full path [string].
+
+            > If return_readable is False:
+                1. backups: list with each backup file's full path
+                            [list of strings].
+    """
+    # Determines the full paths of all backup files
+    backup_paths = os.path.join(glb.BACKUP_DIR, os.listdir(glb.BACKUP_DIR))
+
+    # Checks if the readable option was selected
+    if return_readable:
+        # If so, initializes an empt dictionary
+        paths_dict = {}
+
+        # Loops through each backup file's path, storing it in the dictionary
+        # with a sequential path key (i.e. path_001, path_002, etc ...)
+        for i, pth in enumerate(backup_paths):
+            paths_dict['path_' + str(i).zfill(2)] = pth
+
+        # Returns the more-readable-when-JSON-enconded dictionary
+        return paths_dict
+    else:
+        # Otherwise, returns the backup paths as a list (in a dictionary with a
+        # single element)
+        return {"backups":backup_paths}
+
+# ------------------------------------------------------------------------------
+
 @fr_router.post("/people/list")
 async def people_list(show_hidden: bool = Query(False, description="boolean value to show visible people (false) or ALSO hidden one (true)")):
     """
